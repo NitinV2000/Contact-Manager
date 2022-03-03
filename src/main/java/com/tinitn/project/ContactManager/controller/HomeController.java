27e51599cb3676df0a64c995b9d0bd6ae3e7b0da
NitinV2000 <nitin.vankadari2018@vitstudent.ac.in>
2022-03-03 21:04:07 +0530

    Added forgot password functionality

--- src/main/java/com/tinitn/project/ContactManager/controller/HomeController.java
+++ src/main/java/com/tinitn/project/ContactManager/controller/HomeController.java
@@ -1,8 +1,12 @@
 package com.tinitn.project.ContactManager.controller;
 
+import java.util.Random;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,8 +18,14 @@ import com.tinitn.project.ContactManager.dao.UserRepository;
 import com.tinitn.project.ContactManager.entity.Registration;
 import com.tinitn.project.ContactManager.entity.User;
 
+
 @RestController
 public class HomeController {
+	
+	@Autowired
+	private EmailService service;
+	
+	Random random = new Random(1000);
 	
 	@Autowired
 	private UserRepository userRepo;
@@ -41,5 +51,29 @@ public class HomeController {
 		u.setPassword(passwordEncoder.encode(reg.getPassword()));
 		u.setImageUrl("default.png");
 		return userRepo.save(u);
+	}
+	
+	@PostMapping("/forgot-password")
+	public String sendOtp(@RequestParam("email") String email) {
+		int otp = random.nextInt(999999);
+		service.sendSimpleEmail(email, Integer.toString(otp), "OTP From Contact Manager");
+		return "Sent OTP";
+	}
+	
+	@PostMapping("/verify-otp")
+	public ResponseEntity<?> verify(@RequestParam("email") String email, @RequestParam("otp") String otp, @RequestParam("notp") String notp) {
+		int otp1 = Integer.valueOf(otp);
+		int otp2 = Integer.valueOf(notp);
+		if(otp1==otp2)
+			return new ResponseEntity<>("OTP Matched",HttpStatus.OK);
+		else
+			return new ResponseEntity<>("OTP not matched",HttpStatus.BAD_REQUEST);
+	}
+	
+	@PostMapping("/change-password")
+	public User change(@RequestParam("email") String email,@RequestParam("pass") String pass) {
+		User u = userRepo.getUserByUserName(email);
+		u.setPassword(pass);
+		return u;
 	}
 }
--- src/main/java/com/tinitn/project/ContactManager/controller/UserController.java
+++ src/main/java/com/tinitn/project/ContactManager/controller/UserController.java
@@ -1,16 +1,14 @@
 package com.tinitn.project.ContactManager.controller;
 
-import java.io.File;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.security.Principal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -22,11 +20,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.tinitn.project.ContactManager.dao.ContactRepository;
 import com.tinitn.project.ContactManager.dao.UserRepository;
